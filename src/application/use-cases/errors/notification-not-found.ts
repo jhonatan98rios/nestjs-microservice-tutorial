25a8c6e54e0d8cf89e0bbc3783c6305d3c568dee
Jhonatan Teixeira Rios <jhonatan98rios@gmail.com>
2022-12-16 19:49:17 -0300

    Complete application

---
+++ src/application/use-cases/errors/notification-not-found.ts
@@ -0,0 +1,5 @@
+export class NotificationNotFound extends Error {
+    constructor() {
+        super('Nottification not found!')
+    }
+}
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,6 +2,11 @@ import { Module } from '@nestjs/common'
 import { SendNotification } from '@application/use-cases/send-notification';
 import { DatabaseModule } from '../database/database.module';
 import { NotificationsController } from './controllers/notifications.controller';
+import { CancelNotification } from '@application/use-cases/cancel-notification';
+import { CountRecipientNotifications } from '@application/use-cases/count-recipient-notifications';
+import { GetRecipientNotifications } from '@application/use-cases/get-recipient-notifications';
+import { ReadNotification } from '@application/use-cases/read-notification';
+import { UnreadNotification } from '@application/use-cases/unread-notifications';
 
 @Module({
     imports: [ 
@@ -11,7 +16,12 @@ import { NotificationsController } from './controllers/notifications.controller'
         NotificationsController
     ],
     providers: [
-        SendNotification
+        SendNotification,
+        CancelNotification,
+        CountRecipientNotifications,
+        GetRecipientNotifications,
+        ReadNotification,
+        UnreadNotification
     ]
 })
 export class HttpModule {}
